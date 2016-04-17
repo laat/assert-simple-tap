2db2b1ea268c95aaadf6df64d5868a4008726ce4
Sigurd Fosseng <sigurd@fosseng.net>
2016-04-17 19:50:30 +0200

    initial

---
+++ src/assert.js
@@ -0,0 +1,87 @@
+import native from 'assert'
+import YAML from 'yamljs'
+
+let tests = 0
+let passes = 0
+let failures = 0
+
+console.log('TAP version 13')
+
+function printError (err) {
+  const message = {
+    name: err.name,
+    message: err.message,
+    stack: err.stack.split('\n')
+  }
+  console.log('  ---')
+  console.log(YAML.stringify(message, 4).replace(/^/gm, '  '))
+  console.log('  ...')
+}
+function trace (message) { return function (x) { console.log(message, x); return x; }}; // eslint-disable-line
+const t = trace('debug')
+
+function test (message, fn) {
+  return ((...args) => {
+    tests++
+    try {
+      fn(...args)
+      console.log(`ok ${tests} ${message || ''}`)
+      passes++
+    } catch (err) {
+      console.log(`not ok ${tests} ${message || ''}`)
+      failures++
+      printError(err)
+    }
+  })()
+}
+
+const assert = (value, message) => {
+  return test(message, () => native(value, message))
+}
+assert.deepEqual = (actual, expected, message) => {
+  return test(message, () => native.deepEqual(actual, expected, message))
+}
+assert.deepStrictEqual = (actual, expected, message) => {
+  return test(message, () => native.deepStrictEqual(actual, expected, message))
+}
+assert.doesNotThrow = (block, error, message) => {
+  return test(message, () => native.doesNotThrow(block, error, message))
+}
+assert.equal = (actual, expected, message) => {
+  return test(message, () => native.equal(actual, expected, message))
+}
+assert.fail = (actual, expected, message, operator) => {
+  return test(message, () => native.fail(actual, expected, message, operator))
+}
+assert.ifError = (value) => {
+  return test('', () => native.ifError())
+}
+assert.notDeepEqual = (actual, expected, message) => {
+  return test(message, () => native.notDeepEqual(actual, expected, message))
+}
+assert.notDeepStrictEqual = (actual, expected, message) => {
+  return test(message, () => native.notDeepStrictEqual(actual, expected, message))
+}
+assert.ok = (value, message) => {
+  return test(message, () => native.ok(value, message))
+}
+assert.throws = (block, error, message) => {
+  return test(message, () => native.throws(block, error, message))
+}
+
+module.exports = assert
+
+process.on('uncaughtException', function (err) {
+  console.log(`Bail out! Uncaught exception ${err.name}`)
+  printError(err)
+  process.exit(1)
+})
+
+process.on('exit', function (code) {
+  if (code === 0) {
+    console.log(`1..${tests}`)
+    console.log('# tests ' + tests)
+    console.log('# pass ' + passes)
+    console.log('# fail ' + failures)
+  }
+})
