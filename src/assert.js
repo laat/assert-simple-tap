194089d37529013de9457b6ed60ed565887bd52d
Sigurd Fosseng <sigurd@fosseng.net>
2016-04-19 22:09:18 +0200

    airbnb

--- src/assert.js
+++ src/assert.js
@@ -1,85 +1,94 @@
-const native = require('assert')
-const YAML = require('yamljs')
+/* eslint-disable no-console */
+const native = require('assert');
+const YAML = require('yamljs');
 
-console.log('TAP version 13')
+console.log('TAP version 13');
 
-let tests = 0
-let passes = 0
-let failures = 0
+let tests = 0;
+let passes = 0;
+let failures = 0;
 
-function printError (err) {
+function printError(err) {
   const message = {
     name: err.name,
     message: err.message,
-    stack: err.stack.split('\n')
-  }
-  console.log('  ---')
-  console.log(YAML.stringify(message, 4).replace(/^/gm, '  '))
-  console.log('  ...')
+    stack: err.stack.split('\n'),
+  };
+  console.log('  ---');
+  console.log(YAML.stringify(message, 4).replace(/^/gm, '  '));
+  console.log('  ...');
 }
 
-function test (message, fn) {
+function test(message, fn) {
   return ((...args) => {
-    tests++
+    tests++;
     try {
-      fn(...args)
-      console.log(`ok ${tests} ${message || ''}`)
-      passes++
+      fn(...args);
+      console.log(`ok ${tests} ${message || ''}`);
+      passes++;
     } catch (err) {
-      console.log(`not ok ${tests} ${message || ''}`)
-      failures++
-      printError(err)
+      console.log(`not ok ${tests} ${message || ''}`);
+      failures++;
+      printError(err);
     }
-  })()
+  })();
 }
 
-const assert = (value, message) => {
-  return test(message, () => native(value, message))
-}
+const assert = (value, message) => test(message, () => native(value, message));
+
 assert.deepEqual = (actual, expected, message) => {
-  return test(message, () => native.deepEqual(actual, expected, message))
-}
+  test(message, () => native.deepEqual(actual, expected, message));
+};
+
 assert.deepStrictEqual = (actual, expected, message) => {
-  return test(message, () => native.deepStrictEqual(actual, expected, message))
-}
+  test(message, () => native.deepStrictEqual(actual, expected, message));
+};
+
 assert.doesNotThrow = (block, error, message) => {
-  return test(message, () => native.doesNotThrow(block, error, message))
-}
+  test(message, () => native.doesNotThrow(block, error, message));
+};
+
 assert.equal = (actual, expected, message) => {
-  return test(message, () => native.equal(actual, expected, message))
-}
+  test(message, () => native.equal(actual, expected, message));
+};
+
 assert.fail = (actual, expected, message, operator) => {
-  return test(message, () => native.fail(actual, expected, message, operator))
-}
+  test(message, () => native.fail(actual, expected, message, operator));
+};
+
 assert.ifError = (value) => {
-  return test('', () => native.ifError())
-}
+  test('', () => native.ifError(value));
+};
+
 assert.notDeepEqual = (actual, expected, message) => {
-  return test(message, () => native.notDeepEqual(actual, expected, message))
-}
+  test(message, () => native.notDeepEqual(actual, expected, message));
+};
+
 assert.notDeepStrictEqual = (actual, expected, message) => {
-  return test(message, () => native.notDeepStrictEqual(actual, expected, message))
-}
+  test(message, () => native.notDeepStrictEqual(actual, expected, message));
+};
+
 assert.ok = (value, message) => {
-  return test(message, () => native.ok(value, message))
-}
+  test(message, () => native.ok(value, message));
+};
+
 assert.throws = (block, error, message) => {
-  return test(message, () => native.throws(block, error, message))
-}
+  test(message, () => native.throws(block, error, message));
+};
 
-process.on('uncaughtException', function (err) {
-  console.log(`Bail out! Uncaught exception ${err.name}`)
-  printError(err)
-  process.exit(1)
-})
+process.on('uncaughtException', (err) => {
+  console.log(`Bail out! Uncaught exception ${err.name}`);
+  printError(err);
+  process.exit(1);
+});
 
-process.on('exit', function (code) {
+process.on('exit', (code) => {
   if (code === 0) {
-    console.log(`1..${tests}`)
-    console.log('# tests ' + tests)
-    console.log('# pass ' + passes)
-    console.log('# fail ' + failures)
+    console.log(`1..${tests}`);
+    console.log(`# tests ${tests}`);
+    console.log(`# pass ${passes}`);
+    console.log(`# fail ${failures}`);
   }
-})
+});
 
-module.exports = assert
+module.exports = assert;
--- src/assert.test.js
+++ src/assert.test.js
@@ -1,15 +1,17 @@
 import assert from './assert';
 
-assert(true, 'assert')
-assert.deepEqual(1, 1, 'deepEqual')
-assert.deepStrictEqual(1, 1, 'deepStrictEqual')
-assert.doesNotThrow(() => ({}), Error, 'doesNotThrow')
-assert.equal(1, 1, 'equal')
+assert(true, 'assert');
+assert.deepEqual(1, 1, 'deepEqual');
+assert.deepStrictEqual(1, 1, 'deepStrictEqual');
+assert.doesNotThrow(() => ({}), Error, 'doesNotThrow');
+assert.equal(1, 1, 'equal');
+
 // assert.fail()
 // assert.ifError()
-assert.notDeepEqual(1, 2, 'notDeepEqual')
-assert.notDeepStrictEqual(1, 2, 'notDeepStrictEqual')
-assert.ok(true, 'ok')
+
+assert.notDeepEqual(1, 2, 'notDeepEqual');
+assert.notDeepStrictEqual(1, 2, 'notDeepStrictEqual');
+assert.ok(true, 'ok');
 assert.throws(() => {
-  throw new Error('throws')
-}, Error, 'throws')
+  throw new Error('throws');
+}, Error, 'throws');
