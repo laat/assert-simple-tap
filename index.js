aded67799c608e441640fc69c343d2263cb6d0a3
Sigurd Fosseng <sigurd@fosseng.net>
2019-06-10 14:29:44 +0200

    assert errors tape-like

--- index.js
+++ index.js
@@ -3,21 +3,20 @@
 /* eslint-disable no-console */
 const native = require("assert");
 const tapYaml = require("tap-yaml");
+const StackUtils = require("stack-utils");
 
 console.log("TAP version 13");
 
 let tests = 0;
 let passes = 0;
 let failures = 0;
+const stackUtils = new StackUtils({
+  internals: [...StackUtils.nodeInternals(), /\/assert-simple-tap\/index.js/]
+});
 
-function printError(err) {
-  const message = {
-    name: err.name,
-    message: err.message,
-    stack: err.stack.split("\n")
-  };
+function printObject(obj) {
   console.log("  ---");
-  console.log(tapYaml.stringify(message, 4).replace(/^/gm, "  "));
+  console.log(tapYaml.stringify(obj, 4).replace(/^/gm, "  "));
   console.log("  ...");
 }
 
@@ -32,7 +31,20 @@ function test(message, fn) {
     } catch (err) {
       console.log(`not ok ${tests} ${message || ""}`);
       failures++;
-      printError(err);
+      if ("actual" in err && "expected" in err && "operator" in err) {
+        printObject({
+          operator: err.operator,
+          actual: err.actual,
+          expected: err.expected,
+          at: stackUtils
+            .clean(err.stack)
+            .split("\n")
+            .filter((x, i) => x !== "}")
+            .join("\n")
+        });
+      } else {
+        printObject(err);
+      }
     }
   })();
 }
@@ -81,7 +93,7 @@ assert.throws = (block, error, message) => {
 
 process.on("uncaughtException", err => {
   console.log(`Bail out! Uncaught exception ${err.name}`);
-  printError(err);
+  printObject(err);
   process.exit(1);
 });
 
